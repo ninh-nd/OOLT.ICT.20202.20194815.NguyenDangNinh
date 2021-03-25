a65c54419c52e42815a98af34a61c45f3c8d73ac
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-03-25 07:56:58 +0700

    Initial Lab04 exercise

---
+++ Lab04/AimsProject/src/DateTest.java
@@ -0,0 +1,9 @@
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		MyDate datetest = new MyDate("12", "feb", "2020");
+		datetest.print(datetest);
+		datetest.alternativePrint(datetest);
+	}
+}

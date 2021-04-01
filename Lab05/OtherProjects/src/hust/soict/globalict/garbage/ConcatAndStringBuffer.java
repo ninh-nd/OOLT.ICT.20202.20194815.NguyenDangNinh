110db4a518436f3f497582819e558820e5037781
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-04-01 15:40:54 +0700

    Add week 5 exercises

---
+++ Lab05/OtherProjects/src/hust/soict/globalict/garbage/ConcatAndStringBuffer.java
@@ -0,0 +1,32 @@
+package hust.soict.globalict.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConcatAndStringBuffer {
+	public static String readFile(String fileName) throws Exception {
+		String input = "";
+		input = new String(Files.readAllBytes(Paths.get(fileName)));
+		return input;
+	}
+	public static void main(String[] args) throws Exception {
+		//Using concatenation
+		long start = System.currentTimeMillis();
+		String input = readFile("C:\\Users\\admin\\Desktop\\Eclipse-workspace\\OtherProjects\\src\\hust\\soict\\globalict\\garbage\\textfile.txt");
+		String output = "";
+		for (int i=0; i<input.length(); i++) {
+			char a = input.charAt(i);
+			output += a;
+		}
+		System.out.println(System.currentTimeMillis() - start);
+		//Using string buffer
+		start = System.currentTimeMillis();
+		StringBuilder outputByBuffer = new StringBuilder();
+		for (int i=0; i<input.length(); i++) {
+			char a = input.charAt(i);
+			outputByBuffer.append(a);
+		}
+		System.out.println(System.currentTimeMillis() - start);
+	}
+}
+
---
+++ Lab05/OtherProjects/src/hust/soict/globalict/lab01/SecondDegreeEquation.java
@@ -0,0 +1,28 @@
+package hust.soict.globalict.lab01;
+import java.util.Scanner;
+import java.lang.Math;
+public class SecondDegreeEquation {
+
+	public static void main(String[] args) {
+		double a, b, c;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Program calculate ax^2+bx+c=0. Enter a: ");
+		a = keyboard.nextDouble();
+		while (a == 0) {
+			System.out.print("Please enter a different from 0: ");
+			 a = keyboard.nextDouble();
+		}
+		System.out.print("Enter b: ");
+		b = keyboard.nextDouble();
+		System.out.print("Enter c: ");
+		c = keyboard.nextDouble();
+		double delta = Math.pow(b, 2) - 4*a*c;
+		if (delta < 0) System.out.println("The equation has no real root");
+		else if (delta == 0) System.out.println("This equation has one root: " + -b/(2*a));
+		else {
+			System.out.println("This equation has two roots: ");
+			System.out.println("x1 = " + (-b-Math.sqrt(delta))/(2*a));
+			System.out.println("x2 = " + (-b+Math.sqrt(delta))/(2*a));
+		}
+	}
+}
